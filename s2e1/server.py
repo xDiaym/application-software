179f60d65503fe7e660956e2b4afd86450b0997a
Matthew Nekirov <matthew.nekirov@gmail.com>
2025-09-16 08:37:23 +0300

    add database

--- s2e1/server.py
+++ s2e1/server.py
@@ -1,13 +1,81 @@
 import asyncio
 from collections import defaultdict
+import datetime
 import functools
+import hashlib
 import logging
-import sys
+import os
 import typing as t
 
+import aiosqlite
+
+
+GLOBAL_CHAT_ID = 1
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("server")
+
+
+@functools.lru_cache()
+def sql_script(path: str) -> str:
+    with open(f"sql/{path}") as fp:
+        return fp.read()
+
+
+class SQLiteStorage:
+    _SALT = os.environ.get("IRCLIKE_SALT", "1sud83")
+
+    def __init__(self, conn: aiosqlite.Connection) -> None:
+        self._connection = conn
+
+    async def init_schema(self) -> None:
+        script = sql_script("init.sql")
+        async with self._connection.executescript(script):
+            await self._connection.commit()
+
+    async def store_message(self, author: str, text: str) -> None:
+        author_id: int
+        async with self._connection.execute("SELECT id FROM users WHERE nick = ?", (author, )) as cursor:
+            if author_id := await cursor.fetchone() is None:
+                logger.warning("user %s not found", author)
+                return
+
+        query = "INSERT INTO messages(author_id, channel_id, text_) VALUES (?, ?, ?)"
+        async with self._connection.execute(query, (author_id, GLOBAL_CHAT_ID, text)):
+            await self._connection.commit()
+
+    async def get_by_date_period(self, author: str, start: datetime.datetime, end: datetime.datetime) -> None:
+        raise NotImplementedError
+    
+    async def register(self, nick: str, password: str) -> bool:
+        async with self._connection.execute(
+            "SELECT COUNT(*) FROM users WHERE nick = ?",
+            (nick, )
+        ) as cursor:
+            if result := await cursor.fetchone():
+                if result[0] > 0:
+                    return False
+        
+        async with self._connection.execute(
+            "INSERT INTO users(nick, password_hash) VALUES (?, ?)",
+            (nick, self.hash(password))
+        ):
+            await self._connection.commit()
+        return True
+    
+    async def verify(self, nick: str, password: str) -> bool:
+        async with self._connection.execute(
+            "SELECT COUNT(*) FROM users WHERE nick = ? AND password_hash = ?",
+            (nick, self.hash(password))
+        ) as cursor:
+            if result := await cursor.fetchone():
+                return result[0] == 1
+        return False
+    
+    @classmethod
+    def hash(cls, password: str) -> str:
+        return hashlib.sha3_512((password + cls._SALT).encode()).hexdigest()
+
 
 
 def args_required(num_args: int):
@@ -53,8 +121,9 @@ def parse_command(line: str) -> tuple[str, list[str], str]:
 
 
 class IRCServer:
-    def __init__(self) -> None:
+    def __init__(self, storage: SQLiteStorage) -> None:
         self._channels: dict[str, set[Client]] = defaultdict(set)
+        self._storage = storage
 
     async def broadcast(
         self, channel: str, line: str, exclude: Client | None = None
@@ -84,7 +153,7 @@ class IRCServer:
         COMMANDS: dict[str, t.Callable[[Client, list[str], str], t.Awaitable[None]]] = {
             "JOIN": self._join,
             "QUIT": self._quit,
-            "NICK": self._nick,
+            # "NICK": self._nick,
             "PRIVMSG": self._privmsg,
         }
 
@@ -110,6 +179,7 @@ class IRCServer:
     async def _privmsg(self, client: Client, args: list[str], text: str) -> None:
         channel_name = args[0]
         if channel_name.startswith('#') and channel_name in self._channels:
+            await self._storage.store_message(client.nick, text)
             await self.broadcast(channel_name, f'{client.prefix} PRIVMSG {channel_name} :{text}', exclude=client)
 
     async def run(self, host: str = "0.0.0.0", port: int = 6667) -> None:
@@ -119,11 +189,13 @@ class IRCServer:
             await server.serve_forever()
 
 
-def main() -> int:
-    server = IRCServer()
-    asyncio.run(server.run())
+async def main() -> int:
+    async with aiosqlite.connect("irclike.db") as conn:
+        storage = SQLiteStorage(conn)
+        server = IRCServer(storage)
+        await server.run()
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    asyncio.run(main())
--- s2e1/server_test.py
+++ s2e1/server_test.py
@@ -1,6 +1,22 @@
+import hashlib
 import pytest
+import aiosqlite
 
-from server import args_required
+from server import SQLiteStorage, args_required
+
+
+@pytest.fixture(scope="module")
+async def connection():
+    conn = await aiosqlite.connect(":memory:")
+    yield conn
+    await conn.close()
+
+
+@pytest.fixture()
+async def storage(connection):
+    storage = SQLiteStorage(connection)
+    await storage.init_schema()
+    return storage
 
 
 @pytest.mark.parametrize(
@@ -19,3 +35,37 @@ async def test_args_required(n_args, args, called):
         async def f(self, client, args, text):
             assert called
     await Klass().f(None, args, "")
+
+
+async def test_storage_register(storage, connection):
+    assert await storage.register("nick", "password") == True
+    async with connection.execute("SELECT * FROM users") as cursor:
+        assert await cursor.fetchall() == [(1, 'nick', SQLiteStorage.hash("password"))]
+
+    assert await storage.register("nick", "pa55word") == False
+    async with connection.execute("SELECT COUNT(*) FROM users") as cursor:
+        assert await cursor.fetchall() == [(1, )]
+
+
+@pytest.mark.parametrize(
+    ['nick', 'password', 'is_authenticated'],
+    [
+        ('nick', 'password', True),
+        ('n1ck', 'password', False),
+        ('nick', 'pa55word', False),
+    ]
+)
+async def test_storage_verify(storage, connection, nick, password, is_authenticated):
+    await storage.register("nick", "password")
+    assert await storage.verify(nick, password) == is_authenticated
+
+
+async def test_store_message(storage, connection):
+    await storage.register("nick", "password")
+    await storage.store_message("nick", "text")
+    async with connection.execute("SELECT text_ FROM messages LIMIT 1") as cursor:
+        assert await cursor.fetchone() == ("text", )
+
+    await storage.store_message("n1ck", "text2")
+    async with connection.execute("SELECT COUNT(*) FROM messages") as cursor:
+        assert await cursor.fetchone() == (1, )
